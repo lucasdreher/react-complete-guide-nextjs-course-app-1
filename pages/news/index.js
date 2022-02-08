47fc700f7a397357de288ac488454182103600f3
Lucas Dreher <lucasdreher@gmail.com>
2022-02-08 18:16:35 +0100

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,18 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 const NewsPage = () => {
-	return <h1>The News Page</h1>;
+	return (
+		<Fragment>
+			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/link-1-item">Link 1</Link>
+				</li>
+				<li>Link 2</li>
+			</ul>
+		</Fragment>
+	);
 };
 
 export default NewsPage;
